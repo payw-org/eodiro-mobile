e9a2edd67b8163494be4de9e876923f64da4f920
Jang Haemin <io@jhaemin.com>
2020-03-25 22:44:37 +0900

    config: create global stack navigator screen options

---
+++ src/config/stack-navigator-screen-options.ts
@@ -0,0 +1,8 @@
+import { StackNavigationOptions } from '@react-navigation/stack'
+
+export const EodiroStackNavigationScreenOptions: StackNavigationOptions = {
+  headerTintColor: '#ff3852',
+  headerTitleStyle: {
+    color: '#000',
+  },
+}
