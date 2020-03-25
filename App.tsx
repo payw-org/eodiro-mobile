2c6417ab630d0ab5709d78b89a474e1c9e428eb9
Jang Haemin <io@jhaemin.com>
2020-03-25 22:39:27 +0900

    feat: create two more tabs

--- App.tsx
+++ App.tsx
@@ -4,6 +4,8 @@ import { NavigationContainer } from '@react-navigation/native'
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
 import { enableScreens } from 'react-native-screens'
 import LecturesScreen from '@/screens/lectures/LecturesScreen'
+import { Ionicons } from '@expo/vector-icons'
+import SquareScreen from '@/screens/square/SquareScreen'
 
 enableScreens()
 
@@ -19,11 +21,60 @@ const Tab = createBottomTabNavigator<TabParamList>()
 export default function App() {
   return (
     <NavigationContainer>
-      <Tab.Navigator>
-        {/* <Tab.Screen name="Vacant" component={} /> */}
-        <Tab.Screen name="Lectures" component={LecturesScreen} />
-        {/* <Tab.Screen name="Square" component={} />
-        <Tab.Screen name="More" component={} /> */}
+      <Tab.Navigator
+        tabBarOptions={{
+          activeTintColor: '#ff3852',
+        }}
+      >
+        <Tab.Screen
+          name="Vacant"
+          component={LecturesScreen}
+          options={{
+            title: '빈 강의실',
+            tabBarIcon: (props) => {
+              return (
+                <Ionicons
+                  name="ios-time"
+                  size={props.size}
+                  color={props.color}
+                />
+              )
+            },
+          }}
+        />
+        <Tab.Screen
+          name="Lectures"
+          component={LecturesScreen}
+          options={{
+            title: '강의 검색',
+            tabBarIcon: (props) => {
+              return (
+                <Ionicons
+                  name="ios-search"
+                  size={props.size}
+                  color={props.color}
+                />
+              )
+            },
+          }}
+        />
+        <Tab.Screen
+          name="Square"
+          component={SquareScreen}
+          options={{
+            title: '빼빼로 광장',
+            tabBarIcon: (props) => {
+              return (
+                <Ionicons
+                  name="ios-people"
+                  size={props.size}
+                  color={props.color}
+                />
+              )
+            },
+          }}
+        />
+        {/* <Tab.Screen name="More" component={} /> */}
       </Tab.Navigator>
     </NavigationContainer>
   )
